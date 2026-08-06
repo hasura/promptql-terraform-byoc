2d7c19181621ecda130642bf1402fe99917ec3fa
Shahidh K Muhammed <shahidhkmuhammed@gmail.com>
2026-08-06 15:17:18 -0700

    fix: update modules/aws/byok/main.tf

--- modules/aws/byok/main.tf
+++ modules/aws/byok/main.tf
@@ -40,7 +40,7 @@ data "aws_iam_policy_document" "key_policy" {
     effect = "Allow"
     principals {
       type        = "AWS"
-      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+      identifiers = var.administrator_arns != null ? var.administrator_arns : ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
     }
     actions   = ["kms:*"]
     resources = ["*"]
